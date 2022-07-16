8b818501a86808b432c1c1bf24393a2f15c8212c
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:58:42 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T05Ejercicio17
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[10];
+
+            IngresarValores(array);
+            MostrarIndice(array);
+        }
+
+        static void IngresarValores(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Escriba el valor del array con posición " + (i + 1) + "º");
+                int valor = Convert.ToInt32(Console.ReadLine());
+                array[i] = valor;
+            }
+        }
+
+        static void MostrarIndice(int[] array)
+        {
+            foreach (int x in array)
+            {
+                Console.WriteLine("El elemento del array con índice: [{0}] Tiene el valor: {1}", x - 1, array[x - 1]);
+            }
+        }
+
+    }
+}
